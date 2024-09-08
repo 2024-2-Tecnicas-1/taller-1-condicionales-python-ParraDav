3d3b3175612332fa3b2ab9ae454d1ec0771d5830
ParraDav <df.parra.07@gmail.com>
2024-09-07 21:23:33 -0500

    Tenis y triángulo
    Se añaden los códigos de:
    -Set de tenis
    -Triángulo

--- src/set_de_tenis.py
+++ src/set_de_tenis.py
@@ -1,12 +1,28 @@
-def evaluar(num_victorias_a, num_victorias_b):
+def evaluar(A, B):
     # TODO: Coloca aquí el código del ejercicio 1: Set de tenis
-    return ""
+    if ((A >= 6) | (B >= 6)):
+            if (((A > 7) & (B >= 6))|((B > 7) & (A >= 6))):
+                return "Inválido"
+            
+            if (((A == 7) & (B < 5)) | ((B == 7) & (A < 5))):
+                return "Inválido"
+            
+            if (((A >= 6) & (A <= 7)) & (A >= (B + 2))):
+                return "Ganó A"
+            elif (((B >= 6) & (B <= 7)) & (B >= (A + 2))): 
+                return "Ganó B"
+            
+            if ((A == 7) & (B == 6)):
+                return "Ganó A"
+            elif ((B == 7) & (A == 6)):
+                return "Ganó B"
+    return "Aún no termina"
 
 if __name__ == '__main__':
     print("Los juegos ganaddor por A:", end="")
-    num_victorias_a = int(input())
+    A = int(input())
     print("Los juegos ganaddor por B:", end="")
-    num_victorias_b = int(input())
+    B = int(input())
 
-    respuesta = evaluar(num_victorias_a, num_victorias_b)
+    respuesta = evaluar(A, B)
     print(respuesta)
--- src/triangulo.py
+++ src/triangulo.py
@@ -1,6 +1,13 @@
 def evaluar(a, b, c):
     # TODO: Coloca aquí el código del ejercicio 1: Set de tenis
-    return ""
+    if ((a>= (b+c))|(b>= (a+c))|(c>=(a+b))):
+        return "No es un triángulo válido"
+    elif ((a==b) & (b==c)):
+        return "El triángulo es equilátero"
+    elif (((a==b) & (b!=c))|((a==c)&(c!=b))|((b==c)&(c!=a))):
+        return "El triángulo es isósceles"
+    else:   
+        return "El triángulo es escaleno"
 
 if __name__ == '__main__':
     print("a:", end="")
